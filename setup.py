f762ad483a05747bba51ef1b0effe5283ef2631f
a-tal <github@talsma.ca>
2017-05-15 14:01:25 -0700

    0.14.6

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Cache-Cassandra',
-    version='0.14.5',
+    version='0.14.6',
     url='http://github.com/a-tal/flask-cache-cassandra',
     license='BSD',
     author='Thadeus Burgess',
